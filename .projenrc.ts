d97cec6736a85c470f5fc15f4c03cc9aad664e6d
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-05-01 19:20:20 +0900

    feat: search L1 props

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,9 +4,9 @@ const project = new typescript.TypeScriptProject({
   name: 'cdk-unsupported-property',
   projenrcTs: true,
 
-  // deps: [],                /* Runtime dependencies of this module. */
+  deps: ['@typescript-eslint/parser', 'glob', '@typescript-eslint/typescript-estree'], /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
+  devDeps: ['@types/glob'], /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
 project.synth();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,58 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+// dependencies
+import * as fs from 'fs';
+import * as path from 'path';
+import * as ts from '@typescript-eslint/typescript-estree';
+import { glob } from 'glob';
+
+// 非同期でパターンにマッチするすべてのファイルを検索する関数
+const findTypeScriptFiles = async (srcDir: string): Promise<string[]> => {
+  return glob(`${srcDir}/**/*.generated.ts`);
+};
+
+// TypeScriptファイルを解析し、CfnXxxPropsのプロパティを抽出する関数
+const extractCfnProperties = async (filePath: string): Promise<string[]> => {
+  const code = fs.readFileSync(filePath, 'utf8');
+  const ast = ts.parse(code, {
+    loc: true,
+    tokens: true,
+    comment: true,
+    jsx: false,
+    useJSXTextNode: false,
+  });
+
+  const properties: string[] = [];
+
+  ts.simpleTraverse(ast, {
+    enter(node) {
+      if (node.type === 'TSInterfaceDeclaration' && node.id.name.endsWith('Props')) {
+        node.body.body.forEach((prop: any) => {
+          if (prop.type === 'TSPropertySignature') {
+            properties.push(prop.key.name);
+          }
+        });
+      }
+    },
+  });
+
+  return properties;
+};
+
+const main = async () => {
+  const directoryPath = process.argv[2]; // コマンドライン引数からディレクトリパスを取得
+  if (!directoryPath) {
+    console.error('Please provide the directory path');
+    process.exit(1);
+  }
+
+  try {
+    const files = await findTypeScriptFiles(directoryPath);
+    for (const file of files) {
+      const properties = await extractCfnProperties(file);
+      console.log(`Properties in ${path.basename(file)}:`, properties);
+    }
+  } catch (error) {
+    console.error('Error:', error);
   }
 }
+
+void main();
